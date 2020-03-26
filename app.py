368792549a60b7954152ff2c91dbc340f16fa917
artem-vasiltsov <artemvasiltsov01@gmail.com>
2020-03-26 14:48:08 +0300

    Modified

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, request
 from src.ssim.image_similarity import compare_two_images
 from src.image_url.image_from_url import get_image_from_url
 from utils.image_processing import preprocess_image
+from utils.folder_file_manager import log_print
 from settings import EXP_CONST, SERVER_HOST, SERVER_PORT, CUR_DIR, LOCAL
 
 app = Flask(__name__)
@@ -15,23 +16,30 @@ app = Flask(__name__)
 @app.route('/compare', methods=['GET'])
 def run():
 
-    frame1_url = request.args.get('img1')
-    frame2_url = request.args.get('img2')
+    try:
 
-    frame1_path = get_image_from_url(https_url=frame1_url, file_id="1")
-    frame2_path = get_image_from_url(https_url=frame2_url, file_id="2")
-    processed_frame1_path = preprocess_image(frame_path=frame1_path, file_id="1")
-    processed_frame2_path = preprocess_image(frame_path=frame2_path, file_id="2")
-    _, similarity = compare_two_images(frame1_path=processed_frame1_path, frame2_path=processed_frame2_path)
-    ss = 1 - math.exp(EXP_CONST * similarity)
-    print(similarity)
-    print(ss)
-    data = {'similarity score': ss}
-    response = json.dumps(data)
-    for path in glob.glob(os.path.join(CUR_DIR, 'utils', '*.jpg')):
-        os.remove(path)
+        frame1_url = request.args.get('img1')
+        frame2_url = request.args.get('img2')
 
-    return response
+        frame1_path = get_image_from_url(https_url=frame1_url, file_id="1")
+        frame2_path = get_image_from_url(https_url=frame2_url, file_id="2")
+        processed_frame1_path = preprocess_image(frame_path=frame1_path, file_id="1")
+        processed_frame2_path = preprocess_image(frame_path=frame2_path, file_id="2")
+        _, similarity = compare_two_images(frame1_path=processed_frame1_path, frame2_path=processed_frame2_path)
+        ss = 1 - math.exp(EXP_CONST * similarity)
+        print(similarity)
+        print(ss)
+        data = {'score': ss}
+        response = json.dumps(data)
+        for path in glob.glob(os.path.join(CUR_DIR, 'utils', '*.jpg')):
+            os.remove(path)
+
+        return response
+    except Exception as e:
+        log_print(info_str=e)
+        data = {'score': 0}
+        response = json.dumps(data)
+        return response
 
 
 if __name__ == '__main__':
